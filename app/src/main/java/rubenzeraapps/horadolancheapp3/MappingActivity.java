c4d1925c064bc0177337cb51ae6ba161fb2ec6c5
Rubens Araújo <rubensaviana@hotmail.com>
2017-12-06 18:21:13 -0300

    Adicionado comunicacao com backend na parte do mapa

--- app/src/main/java/rubenzeraapps/horadolancheapp3/MappingActivity.java
+++ app/src/main/java/rubenzeraapps/horadolancheapp3/MappingActivity.java
@@ -1,7 +1,10 @@
 package rubenzeraapps.horadolancheapp3;
 
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -11,10 +14,20 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MappingActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private String TAG = MappingActivity.class.getSimpleName();
+    private ListView lv;
+
+    ArrayList<HashMap<String, String>> userList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +37,93 @@ public class MappingActivity extends FragmentActivity implements OnMapReadyCallb
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        //new DownloadJsonAsyncTask().execute("http://127.0.0.1:5000/");
+        userList = new ArrayList<>();
+        //lv = (ListView) findViewById(R.id.list);
+
+        new GetUser().execute();
+    }
+
+    private class GetUser extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(MappingActivity.this,"Json Data is downloading", Toast.LENGTH_LONG).show();
+
+        }
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HTTPClient sh = new HTTPClient();
+            // Making a request to url and getting response
+            String url = "http://127.0.0.1:5000/users";
+            String jsonStr = sh.makeServiceCall(url);
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting JSON Array node
+                    JSONArray users = jsonObj.getJSONArray("users");
+
+                    // looping through All Contacts
+                    for (int i = 0; i < users.length(); i++) {
+                        JSONObject c = users.getJSONObject(i);
+                        String id = c.getString("id");
+                        String name = c.getString("name");
+                        String email = c.getString("email");
+                        String username = c.getString("username");
+                        String longitude = c.getString("longitude");
+                        String latitude = c.getString("latitude");
+                        String accountImage = c.getString("account_image");
+                        String password = c.getString("password");
+
+                        /*// Phone node is JSON Object
+                        JSONObject phone = c.getJSONObject("phone");
+                        String mobile = phone.getString("mobile");
+                        String home = phone.getString("home");
+                        String office = phone.getString("office");*/
+
+                        // tmp hash map for single contact
+                        HashMap<String, String> user = new HashMap<>();
+
+                        // adding each child node to HashMap key => value
+                        user.put("id", id);
+                        user.put("name", name);
+                        user.put("email", email);
+                        user.put("password", password);
+
+                        // adding contact to contact list
+                        userList.add(user);
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                }
+
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            return null;
+        }
+
     }
 
     /**
